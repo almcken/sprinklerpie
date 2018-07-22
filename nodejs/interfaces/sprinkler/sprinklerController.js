35d57677701397803f8eb8807d10d45a211953f2
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-07-21 21:31:08 -0700

    Add initial config for touchOSC for manual operation

--- nodejs/interfaces/sprinkler/sprinklerController.js
+++ nodejs/interfaces/sprinkler/sprinklerController.js
@@ -11,6 +11,26 @@ function SprinklerController() {
 	rpio.open(12, rpio.OUTPUT, rpio.LOW);
 }
 
+function configTouchOSC() {
+	const osc = require('node-osc');
+	const oscServer = new osc.Server(3000, '0.0.0.0');
+	const client = new osc.Client('192.168.0.101', 3333);
+	oscServer.on('message', function (message, rinfo) {
+		switch (message[0]) {
+			case '/1/toggle4':
+				if (message[1] == '1') {
+					startSprinkler()
+				} else {
+					stopSprinkler()
+				}
+				break;
+			case '/ping':
+				console.log('ping')
+				break;
+		}
+	});
+}
+
 /*
 * Private helper function
 */
